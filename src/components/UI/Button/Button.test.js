95dafb8d29b167a7bec269b9e8bf15b8528d010a
McPixeI <torres.88.bcn@gmail.com>
2022-05-02 19:34:23 +0200

    Button unit tests

--- src/components/UI/Button/Button.test.js
+++ src/components/UI/Button/Button.test.js
@@ -3,23 +3,31 @@ import React from 'react'
 import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import { Button } from './Button'
 
-const clickEvent = jest.fn()
-
-const Component = (props) =>
-  <Button
-    {...props}
-    id='test'
-  >
-    Test text
-  </Button>
+const mockFn = jest.fn()
 
 describe('Button', () => {
-  test('Button | Renders without errors', async () => {
-    render(Component)
+  it('Button | Should render without errors', () => {
+    render(<Button>hello</Button>)
+    expect(screen.getByRole('button', { name: /hello/i })
+    ).toBeVisible()
   })
-  test('Button | Onclick event is working', async () => {
-    const btnElement = screen.getByRole('button', { name: 'Test text' })
-    fireEvent.click(btnElement)
-    await waitFor(() => expect(clickEvent).toHaveBeenCalled())
+
+  it('Button | Should fires click event properly', async () => {
+    render(<Button onClick={mockFn}>hello</Button>)
+    const btn = screen.getByRole('button', { name: /hello/i })
+    fireEvent.click(btn)
+    await waitFor(() => expect(mockFn).toHaveBeenCalled())
+  })
+
+  it('Button | Should be disabled when loading state', () => {
+    render(<Button loading>hello</Button>)
+    const btn = screen.getByRole('button', { name: /hello/i })
+    expect(btn).toBeDisabled()
+  })
+
+  it('Button | Should be disabled when disabled state', () => {
+    render(<Button disabled>hello</Button>)
+    const btn = screen.getByRole('button', { name: /hello/i })
+    expect(btn).toBeDisabled()
   })
 })
