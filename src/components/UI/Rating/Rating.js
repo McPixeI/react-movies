e1764542426fd8b82c6d5d942143654dcb263e86
McPixeI <torres.88.bcn@gmail.com>
2022-05-03 15:34:05 +0200

    changed rating component from svgs to heroicons

--- src/components/UI/Rating/Rating.js
+++ src/components/UI/Rating/Rating.js
@@ -1,18 +1,13 @@
+import { StarIcon } from '@heroicons/react/solid'
+import { StarIcon as StarIconOutline } from '@heroicons/react/outline'
+
 export const Rating = ({ count }) => {
   const filledStars = parseInt(count) * 5 / 10
   return [...new Array(5)].map((val, i) => {
     if (i <= filledStars - 1) {
-      return (
-        <svg key={`star-${i}`} fill='currentColor' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' className='w-4 h-4 text-primary' viewBox='0 0 24 24'>
-          <path d='M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z' />
-        </svg>
-      )
+      return <StarIcon className='w-6 h-6 text-primary' />
     } else {
-      return (
-        <svg key={`star-${i}`} fill='none' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' className='w-4 h-4 text-primary' viewBox='0 0 24 24'>
-          <path d='M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z' />
-        </svg>
-      )
+      return <StarIconOutline className='w-5 h-5 text-primary' />
     }
   })
 }
